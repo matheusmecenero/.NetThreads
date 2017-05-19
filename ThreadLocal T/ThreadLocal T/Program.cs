6e148c55cc14ba4928232d37153fc8207a7bb54b
matheusmecenero <mat.mecenero@gmail.com>
2017-05-19 02:35:20 -0300

    ThreadLocalT

--- ThreadLocal T/ThreadLocal T/Program.cs
+++ ThreadLocal T/ThreadLocal T/Program.cs
@@ -9,6 +9,15 @@ namespace ThreadLocalT
         public static ThreadLocal<int> _field =
             new ThreadLocal<int>(() =>
             {
+
+                //Here you see another feature of the.NET Framework. You can use the Thread.CurrentThread class to ask for information about the thread that’s executing.
+                //This is called the thread’s execution context.This property gives you access to properties like the thread’s current culture (a CultureInfo associated with 
+                //the current thread that is used to format dates, times, numbers, currency values, the sorting order of text, casing conventions, and string comparisons), 
+                //principal(representing the current security context), priority(a value to indicate how the thread should be scheduled by the operating system), and other info.
+                //When a thread is created, the runtime ensures that the initiating thread’s execution context is flowed to the new thread.This way the new thread has 
+                //the same privileges as the parent thread.
+                //This copying of data does cost some resources, however. If you don’t need this data, you can disable this behavior by using the ExecutionContext.SuppressFlow method.
+
                 return Thread.CurrentThread.ManagedThreadId;
             });
 
