4e4b6f484101b939a1c956abb0613f7d73deb9c9
matheusmecenero <mat.mecenero@gmail.com>
2017-05-19 14:28:41 -0300

    QueueThreadPool

---
+++ QueueThreadPool/QueueThreadPool/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace QueueThreadPool
+{
+    class Program
+    {
+        public static void Main()
+        {
+            ThreadPool.QueueUserWorkItem((s) =>
+            {
+                Console.WriteLine("Working on a thread from threadpool");
+            });
+            Console.ReadLine();
+        }
+    }
+}
