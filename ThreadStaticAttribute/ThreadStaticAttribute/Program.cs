4401ac69330e5055f6e7fb872447046e986642e7
matheusmecenero <mat.mecenero@gmail.com>
2017-05-19 02:19:54 -0300

    ThreadStaticAttribute

--- ThreadStaticAttribute/ThreadStaticAttribute/Program.cs
+++ ThreadStaticAttribute/ThreadStaticAttribute/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Threading;
-namespace ThreadStaticAttribute
+namespace ThreadAttributeStatic
 {
     public static class Program
     {
+        //A thread can also have its own data that’s not a local variable.By marking a field with
+        //the ThreadStatic attribute, each thread gets its own copy of a field
         [ThreadStatic]
         public static int _field;
         public static void Main()
@@ -21,11 +23,14 @@ namespace ThreadStaticAttribute
                 {
                     for(int x = 0; x< 10; x++)
                     {
-                    _field++;
-                    Console.WriteLine("Thread B: {0}", _field);
+                        _field++;
+                        Console.WriteLine("Thread B: {0}", _field);
                     }
                 }).Start();
             Console.ReadKey();
+
+            //With the ThreadStaticAttribute applied, the maximum value of _field becomes 10.
+            //If you remove it, you can see that both threads access the same value and it becomes 20.
         }
     }
 }
