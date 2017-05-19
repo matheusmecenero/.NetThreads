4e6b9dd119c7ef55b42ca7bb8e8290ae8ccb9e7f
matheusmecenero <mat.mecenero@gmail.com>
2017-05-19 00:51:59 -0300

    BackgroundThread comentários

--- BackgroundThread/BackgroundThread/Program.cs
+++ BackgroundThread/BackgroundThread/Program.cs
@@ -5,6 +5,10 @@ namespace BackgroundThread
 {
     public static class Program
     {
+        //Another thing that’s important to know about threads is the difference between foreground
+        //and background threads.Foreground threads can be used to keep an application
+        //alive.Only when all foreground threads end does the common language runtime (CLR) shut
+        //down your application.Background threads are then terminated.
         public static void ThreadMethod()
         {
             for (int i = 0; i < 10; i++)
@@ -16,6 +20,10 @@ namespace BackgroundThread
         public static void Main()
         {
             Thread t = new Thread(new ThreadStart(ThreadMethod));
+
+            //If you run this application with the IsBackground property set to true, the application exits
+            //immediately.If you set it to false(creating a foreground thread), the application prints the
+            //ThreadProc message ten times.
             t.IsBackground = true;
             t.Start();
             Console.ReadLine();
