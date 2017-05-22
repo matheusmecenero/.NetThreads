e29f61c337cad6009f0ef39d29ea8279697c673c
matheusmecenero <mat.mecenero@gmail.com>
2017-05-21 23:55:48 -0300

    UsingATaskThatReturnsAValue

--- 1.9 UsingATaskThatReturnsAValue/UsingATaskThatReturnsAValue/Program.cs
+++ 1.9 UsingATaskThatReturnsAValue/UsingATaskThatReturnsAValue/Program.cs
@@ -10,10 +10,17 @@ namespace UsingATaskThatReturnsAValue
     {
         public static void Main()
         {
-            Task<int> t = Task.Run(() =>
+            //Next to Task, the.NET Framework also has the Task< T > class that you can use if a 
+            //Task should return a value.
+             Task<int> t = Task.Run(() =>
             {
                 return 42;
             });
+
+            //Attempting to read the Result property on a Task will force the thread that’s trying to read the 
+            //result to wait until the Task is finished before continuing. As long as the Task has not finished, 
+            //it is impossible to give the result. If the Task is not finished, this call will block the 
+            //current thread.
             Console.WriteLine(t.Result); // Displays 42
             Console.ReadKey();
         }
