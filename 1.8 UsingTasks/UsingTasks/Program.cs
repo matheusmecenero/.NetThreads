bd86bedb5339a39733442632d677289be0c4e8a1
matheusmecenero <mat.mecenero@gmail.com>
2017-05-21 23:20:11 -0300

    UsingTasks

---
+++ 1.8 UsingTasks/UsingTasks/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+namespace UsingTasks
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            //Queuing a work item to a thread pool can be useful, but it has its shortcomings.There is no
+            //built -in way to know when the operation has finished and what the return value is.
+
+            //This is why the.NET Framework introduces the concept of a Task, which is an object that 
+            //represents some work that should be done.The Task can tell you if the work is completed 
+            //and if the operation returns a result, the Task gives you the result.
+
+            Task t = Task.Run(() =>
+            {
+                for (int x = 0; x < 100; x++)
+                {
+                    Console.Write('*');
+                }
+            });
+            t.Wait();
+
+            Console.ReadKey();
+            //This example creates a new Task and immediately starts it. Calling Wait is equivalent to 
+            //calling Join on a thread. It waits till the Task is finished before exiting the application.
+        }
+    }
+}
